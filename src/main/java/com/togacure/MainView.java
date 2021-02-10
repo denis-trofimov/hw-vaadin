966502df5a85a1b062841ef560f88936bd42e6ef
denis-trofimov <yasebenravlyus@gmail.com>
2021-02-10 21:39:31 +0100

    spring security example implementation

---
+++ src/main/java/com/togacure/MainView.java
@@ -0,0 +1,34 @@
+package com.togacure;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
+import lombok.val;
+
+/**
+ * @author Denis T
+ * @since 10.02.2021
+ */
+@Route
+public class MainView extends AppLayout {
+
+    public MainView() {
+        val logo = new H3("Hello World!");
+        logo.addClassName("logo");
+
+        val logout = new Anchor("logout", "Log out");
+
+        val header = new HorizontalLayout(new DrawerToggle(), logo, logout);
+        header.expand(logo);
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.setWidth("100%");
+        header.addClassName("header");
+
+        addToNavbar(header);
+    }
+
+}
